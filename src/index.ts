ea407e7aec598336130231a693cbc96271630436
Jieren Chen <jieren.chen@gmail.com>
2025-02-12 09:48:47 -0500

    export these too

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { Button } from './components/Button'
 export { ButtonClean } from './components/ButtonClean/ButtonClean'
+
+export { GlobalStyle } from './assets/GlobalStyle'
+export { PPTelegrafFont } from './assets/PPTelegrafFont/PPTelegrafFont'
+export { SuisseIntlFont } from './assets/SuisseIntlFont/SuisseIntlFont'
+export { Colors } from './colors'
+
